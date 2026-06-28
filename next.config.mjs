c8af79846d6ae8e6e5cd017ab34ca7515d73b2bc
masum-k <maasumkamal@gmail.com>
2026-06-29 02:11:21 +0600

    fixed some error

--- next.config.mjs
+++ next.config.mjs
@@ -10,9 +10,7 @@ const nextConfig = {
       },
     ],
   },
-  experimental: {
-    serverComponentsExternalPackages: ['@better-auth/kysely-adapter'],
-  },
+  serverComponentsExternalPackages: ['@better-auth/kysely-adapter'],
 };
 
 export default nextConfig;
